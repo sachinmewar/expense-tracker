2ff1013aebc116bd2d2580d602b3af8963075190
unknown <sachinmewar800@gmail.com>
2021-05-06 16:25:12 +0530

    will  call ExpenseForm to display where user can input

---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NewExpense.css';
+import './ExpenseForm';
+
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = () => {
+   return(
+      <div className= 'new-expense'>
+         <ExpenseForm />
+      </div> 
+   );
+};
+
+export default NewExpense;

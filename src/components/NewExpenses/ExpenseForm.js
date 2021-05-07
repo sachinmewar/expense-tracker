f99931200cf1f73f4b2e99892d3c97ed6bd09e54
unknown <sachinmewar800@gmail.com>
2021-05-07 10:36:36 +0530

    Removing default feature in which clicking button inside the form the page reloads in ExpenseForm.js

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-   return(
-   <form> 
-      <div className= 'new-expense__controls'>
-        <div className = 'new-expense__control'>
-            <label> Title </label>
-            <input type= "text" />
-         </div>
+  // This event was automatically passed by form button.
+  //Everytime we click on button inside the form the page reloads. Removing this defaut feature.
+  const clickButtonHandler = (event) => {
+    event.preventDefault();
+  };
 
-         <div className='new-expense__control'>
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label> Title </label>
+          <input type="text" />
+        </div>
+
+        <div className='new-expense__control'>
           <label>Amount</label>
           <input type='number' min='0.01' step='0.01' />
         </div>
@@ -18,14 +24,14 @@ const ExpenseForm = () => {
         <div className='new-expense__control'>
           <label>Date</label>
           <input type='date' min='2020-01-01' max='2025-12-31' />
-        </div> 
+        </div>
       </div>
-      
+
       <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+        <button type='submit' onClick={clickButtonHandler}>Add Expense</button>
       </div>
-   </form>
-   );
+    </form>
+  );
 }
 
 export default ExpenseForm;

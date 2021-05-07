cf2deb4d3a7ecc540bac98c38ec3e638806b9615
unknown <sachinmewar800@gmail.com>
2021-05-07 11:15:17 +0530

    Added onChange in input feilds which will log input feild value in console

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -8,22 +8,34 @@ const ExpenseForm = () => {
     event.preventDefault();
   };
 
+  const titleHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  const amountHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  const dateHandler = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label> Title </label>
-          <input type="text" />
+          <input type="text" onChange={titleHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={amountHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2020-01-01' max='2025-12-31' />
+          <input type='date' min='2020-01-01' max='2025-12-31' onChange={dateHandler} />
         </div>
       </div>
 

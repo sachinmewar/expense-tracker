30f1dc54007ce358f47d0211a3a2ed77c6972212
unknown <sachinmewar800@gmail.com>
2021-05-08 16:36:50 +0530

    Completed ExpenseFilter which will filter on the basis of year

--- src/components/ExpenseFilter.js
+++ src/components/ExpenseFilter.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
+import './ExpenseFilter.css';
 
+const ExpenseFilter = (props) => {
 
-const ExpenseFilter = () => {
+   const onClickExpenseFilterHandler = (event) => {
+      props.getExpenseFilterValue(event.target.value);
+   };
    return (
-      <div>
-         Filter by year
-         <select>
-            <option value='2020'> 2020 </option>
-            <option value='2021'> 2021 </option>
-            <option value='2022'> 2022 </option>
-            <option value='2023'> 2023 </option>
-         </select>
+      <div className='expense-filter-container'>
+         <div className='expense-filter'>
+            <label> Filter by Year: </label>
+            <select value={props.value} onChange={onClickExpenseFilterHandler}>
+               <option value='2020'> 2020 </option>
+               <option value='2021'> 2021 </option>
+               <option value='2022'> 2022 </option>
+               <option value='2023'> 2023 </option>
+            </select>
+         </div>
       </div>
    );
 };

9f7b05886962817e299241d7b853b589a6f87f88
unknown <sachinmewar800@gmail.com>
2021-05-09 11:31:57 +0530

    App.js received input data from NewExpense.js and send it to expense.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses/NewExpenses/Expenses';
-import NewExpense from './components/Expenses/NewExpenses/NewExpense';
+import { useState } from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpenses/NewExpense';
+
+const dummy_expenses = [
+  { title: 'laptop', amount: 44000, date: new Date() },
+
+  { title: 'mouse', amount: 800, date: new Date() },
+
+  { title: 'keyboard', amount: 1400, date: new Date() },
+
+  { title: 'printer', amount: 6500, date: new Date() }
+];
 
 const App = () => {
-  const expenses = [
-    { title: 'laptop', amount: 44000, date: new Date() },
 
-    { title: 'mouse', amount: 800, date: new Date() },
+  const [newExpense, newExpenseHandler] = useState(dummy_expenses);
+  const addNewExpenseHandler = addExpense => {
+    // Here prevExpense is a default parameter in which prevState is returned
+    // This will make sure that we get the updated state and not outdated state. 
+    newExpenseHandler((prevExpense) => {
+      return [addExpense, ...prevExpense];
+    });
+    //console.log(newExpense);
+  }
 
-    { title: 'keyboard', amount: 1400, date: new Date() },
-
-    { title: 'printer', amount: 6500, date: new Date() }
-  ];
   return (
     <div>
-      <NewExpense />
-      <Expenses expense={expenses} />
+      <NewExpense addNewExpense={addNewExpenseHandler} />
+      <Expenses expense={newExpense} />
     </div>
   );
 }

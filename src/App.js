a7e8d96706640e8fbc6c06b53485795451ff145f
unknown <sachinmewar800@gmail.com>
2021-05-09 09:30:38 +0530

    Added all components related to expenses in Expenses directory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
-import NewExpense from './components/NewExpenses/NewExpense';
+import Expenses from './components/Expenses/NewExpenses/Expenses';
+import NewExpense from './components/Expenses/NewExpenses/NewExpense';
 
 const App = () => {
   const expenses = [
-    {title: 'laptop',amount: 44000, date: new Date()},
-    
-    {title: 'mouse', amount: 800, date: new Date()},
-    
-    {title: 'keyboard', amount: 1400, date: new Date()},
+    { title: 'laptop', amount: 44000, date: new Date() },
 
-    {title: 'printer', amount: 6500, date: new Date()}
-];
-  return(
+    { title: 'mouse', amount: 800, date: new Date() },
+
+    { title: 'keyboard', amount: 1400, date: new Date() },
+
+    { title: 'printer', amount: 6500, date: new Date() }
+  ];
+  return (
     <div>
-      <NewExpense />  
-      <Expenses expense={expenses}/>
+      <NewExpense />
+      <Expenses expense={expenses} />
     </div>
   );
 }

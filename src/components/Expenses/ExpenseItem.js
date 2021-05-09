7a59590b17d1b6ed26e4f0f1e8033cd831aa377e
unknown <sachinmewar800@gmail.com>
2021-05-09 11:32:59 +0530

    removed useState from ExpenseItem as it is not now needed there

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-   const [title, setTitle] = useState(props.title);
-   const [date, setDate] = useState(props.date);
-   const [amount, setAmount] = useState(props.amount);
+   // const [title, setTitle] = useState(props.title);
+   // const [date, setDate] = useState(props.date);
+   // const [amount, setAmount] = useState(props.amount);
 
 
    return (
       <div className="expense-item">
-         <ExpenseDate date={date} />
+         <ExpenseDate date={props.date} />
 
          <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
          </div>
 
-         <div className="expense-item__price">{amount}</div>
+         <div className="expense-item__price">{props.amount}</div>
       </div>
    );
 }

95f443165267ebba5cf45f1b50a0ae4e48d4bbbd
unknown <sachinmewar800@gmail.com>
2021-05-10 19:25:10 +0530

    removed props and added destructuring in ExpenseFilter.js

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,15 +1,15 @@
 import './ExpenseFilter.css';
 
-const ExpenseFilter = (props) => {
+const ExpenseFilter = ({ value, getExpenseFilterValue }) => {
 
    const onClickExpenseFilterHandler = (event) => {
-      props.getExpenseFilterValue(event.target.value);
+      getExpenseFilterValue(event.target.value);
    };
    return (
       <div className='expense-filter-container'>
          <div className='expense-filter'>
             <label> Filter by Year: </label>
-            <select value={props.value} onChange={onClickExpenseFilterHandler}>
+            <select value={value} onChange={onClickExpenseFilterHandler}>
                <option value='2020'> 2020 </option>
                <option value='2021'> 2021 </option>
                <option value='2022'> 2022 </option>

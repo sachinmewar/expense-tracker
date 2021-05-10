6b12402be5fa7031d105e8412e435735fb3a94c3
unknown <sachinmewar800@gmail.com>
2021-05-10 20:15:27 +0530

    Added array of amount and year also added filter by list of amount UI

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,19 +1,61 @@
+import { useState } from 'react';
 import './ExpenseFilter.css';
 
+const filterAmount = [
+   { label: 'Below-500', value: 'Below-500' },
+   { label: '500-999', value: '500-999' },
+   { label: '1000-1499', value: '1000-1499' },
+   { label: '1500-1999', value: '1500-1999' },
+   { label: '2000 and above', value: '2000 and above' }
+];
+
+const filterYear = [
+   { label: '2020', value: '2020' },
+   { label: '2021', value: '2021' },
+   { label: '2022', value: '2022' },
+   { label: '2023', value: '2023' },
+   { label: '2024', value: '2024' }
+];
 const ExpenseFilter = ({ value, getExpenseFilterValue }) => {
 
-   const onClickExpenseFilterHandler = (event) => {
-      getExpenseFilterValue(event.target.value);
+   const [toggleState, setToggleState] = useState();
+
+   const onClickFilterByAmountHandler = () => {
+      setToggleState(true);
    };
+   const onClickFilterByDateHandler = () => {
+      setToggleState(false);
+   }
+   const onClickExpenseFilterHandler = (event) => {
+      getExpenseFilterValue([event.target.value, toggleState]);
+   };
+
    return (
       <div className='expense-filter-container'>
+         <div className='expense-filter-toggle'>
+            <div>
+               <p> Click to filter by amount </p>
+               <button onClick={onClickFilterByAmountHandler}> Click </button>
+            </div>
+            <div>
+               <p> Click to filter by date </p>
+               <button onClick={onClickFilterByDateHandler}> Click </button>
+            </div>
+         </div>
+         <div className='expense-filter'>
+            <label>Filtered by Amount </label>
+            <select>
+               {filterAmount.map(amount =>
+                  <option value={amount.value}> {amount.label} </option>
+               )}
+            </select>
+         </div>
          <div className='expense-filter'>
             <label> Filter by Year: </label>
             <select value={value} onChange={onClickExpenseFilterHandler}>
-               <option value='2020'> 2020 </option>
-               <option value='2021'> 2021 </option>
-               <option value='2022'> 2022 </option>
-               <option value='2023'> 2023 </option>
+               {filterYear.map(year =>
+                  <option value={year.value}> {year.label} </option>
+               )}
             </select>
          </div>
       </div>

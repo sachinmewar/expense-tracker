cb107b25e5ebca15a419254e75521ca29c929e2e
unknown <sachinmewar800@gmail.com>
2021-05-10 11:17:53 +0530

    Expense.js will now show no item found if there is no item in filtered year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,24 +15,24 @@ const Expenses = (props) => {
       return item.date.getFullYear().toString() === filteredYear;
    });
 
+   let expenseDisplay = <p> No Expenses found. </p>
+   if (filteredExpenses.length > 0) {
+      // Added key so that the react can distinguish between different div tags and will not update 
+      // all items and the whole array
+      expenseDisplay = filteredExpenses.map((item) => (
+         <ExpenseItem
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+         />
+      ))
+   }
+
    return (
       <div className="expenses">
          <ExpenseFilter value={filteredYear} getExpenseFilterValue={getExpenseFilterValueHandler} />
-         {
-            // Added key so that the react can distinguish between different div tags and will not update 
-            // all items and the whole array
-            filteredExpenses.map((item) => {
-               return (
-                  <ExpenseItem
-                     key={item.id}
-                     title={item.title}
-                     date={item.date}
-                     amount={item.amount}
-                  />
-               )
-            })
-         }
-
+         {expenseDisplay}
       </div>
    );
 }

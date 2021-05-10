71e5e6afbd854d439bd175d0b36a3f0acf973c18
unknown <sachinmewar800@gmail.com>
2021-05-10 11:55:16 +0530

    added ExpenseList which will display message item not found if array size is zero else call ExpenseItem

---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,28 @@
+import ExpenseItem from './ExpenseItem';
+import './ExpenseList.css';
+
+const ExpenseList = (props) => {
+   const filteredExpenses = props.item;
+   let expenseDisplay = <h2 className="expenses-list__heading"> No Expenses found. </h2>
+   if (filteredExpenses.length > 0) {
+      // Added key so that the react can distinguish between different div tags and will not update 
+      // all items and the whole array
+      expenseDisplay = filteredExpenses.map((item) => (
+         <ExpenseItem
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+         />
+      ))
+   }
+
+   return (
+      <ul className="expense-list">
+         {expenseDisplay}
+      </ul>
+   );
+
+};
+
+export default ExpenseList;

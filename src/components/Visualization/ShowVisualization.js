072910d1a413447434e83680875854d2e8f26e54
unknown <sachinmewar800@gmail.com>
2021-05-16 18:44:23 +0530

    Remove button from showVisualization.js

--- src/components/Visualization/ShowVisualization.js
+++ src/components/Visualization/ShowVisualization.js
@@ -1,26 +1,9 @@
 import Visualization from "./Visualization";
-import { useState } from 'react';
-const ShowVisualization = ({ data }) => {
-   const [buttonState, setButtonState] = useState(false);
 
-   const setChartShownHandler = () => {
-      const textId = document.getElementById('expense-d3chart-button__text');
-      const textContent = textId.textContent;
-      textContent === "Show Chart with D3 Visualization" ?
-         textId.innerHTML = "Hide Chart with D3 Visualization" :
-         textId.innerHTML = "Show Chart with D3 Visualization"
-      setButtonState(
-         buttonState === true ? false : true
-      );
-   };
+const ShowVisualization = ({ data }) => {
    return (
       <div>
-         <button className="expense-d3chart__button" onClick={setChartShownHandler}>
-            <p id="expense-d3chart-button__text" >Show Chart with D3 Visualization</p>
-         </button>
-         { buttonState &&
-            <Visualization data={data} />
-         }
+         <Visualization data={data} />
       </div>
    );
 };

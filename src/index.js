6cc9a0d55bfd908a2babdff53a4f26fb0e3ba627
unknown <sachinmewar800@gmail.com>
2021-05-16 18:45:14 +0530

    Added BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './components/Theme/ThemeContext';
 
-ReactDOM.render(<ThemeProvider><App /></ThemeProvider>, document.getElementById('root'));
+ReactDOM.render(
+   <ThemeProvider>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </ThemeProvider>
+   ,
+   document.getElementById('root'));

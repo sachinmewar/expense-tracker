24f1b243a6f05b665524016b76917680f78d45d9
unknown <sachinmewar800@gmail.com>
2021-05-18 20:44:30 +0530

    Added profile login and logout in Header

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,21 +1,63 @@
 import { NavLink } from 'react-router-dom';
 import classes from './MainHeader.module.css';
+import { useContext } from 'react';
+import AuthContext from '../store/auth-context';
 
 const MainHeader = () => {
+   const authContext = useContext(AuthContext);
+
+   const logoutHandler = () => {
+      authContext.logout();
+   };
    return (
       <header className={classes.header}>
          <nav>
             <ul>
-               <li>
-                  <NavLink to="/" activeClassName={classes.active} > Home </NavLink>
-               </li>
 
-               <li>
-                  <NavLink to="/visualization" activeClassName={classes.active}> Visualization </NavLink>
-               </li>
+               {
+                  !authContext.isLoggedIn &&
+                  < li >
+                     <NavLink to='/auth'> Login </NavLink>
+                  </li>
+               }
+
+               {
+                  authContext.isLoggedIn &&
+                  <li>
+                     <NavLink to="/expenses" activeClassName={classes.active} > Home </NavLink>
+                  </li>
+               }
+
+               {
+                  authContext.isLoggedIn &&
+                  <li>
+                     <NavLink to="/profile" activeClassName={classes.active} > Profile </NavLink>
+                  </li>
+               }
+
+               {
+                  authContext.isLoggedIn &&
+                  <li>
+                     <NavLink to="/visualization" activeClassName={classes.active}> Visualization </NavLink>
+                  </li>
+               }
+
+               {
+                  !authContext.isLoggedIn &&
+                  < li >
+                     <NavLink to="/auth" activeClassName={classes.active}> </NavLink>"
+                  </li>
+               }
+
+               {
+                  authContext.isLoggedIn &&
+                  <li>
+                     <button onClick={logoutHandler}>Logout</button>
+                  </li>
+               }
             </ul>
          </nav>
-      </header>
+      </header >
    );
 };
 

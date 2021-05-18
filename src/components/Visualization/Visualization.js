b69be50c1a686870dd74441305d0f2371bc8403a
unknown <sachinmewar800@gmail.com>
2021-05-18 23:23:38 +0530

    Added Array which stores value of graph now, also added SelectVisualization Component in return

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -1,6 +1,7 @@
-
+import { useState } from 'react';
 import Bar from './Bar';
-import data from '../../Data/Data'
+import data from '../../Data/data';
+import SelectVisualization from './SelectVisualization';
 
 const Visualization = () => {
    const sorted_data = data.sort(function (a, b) {
@@ -8,18 +9,49 @@ const Visualization = () => {
    });
    const lowestValues = sorted_data.slice(0, 5);
    const highestValues = sorted_data.slice(Math.max(sorted_data.length - 5, 0)).reverse();
+
+   const barChartData = [
+      {
+         'data': sorted_data,
+         'chartWidth': 1400,
+         'title': "All Expenses"
+      },
+      {
+         'data': lowestValues,
+         'chartWidth': 1000,
+         'title': "Lowest 5 Expenses"
+      },
+      {
+         'data': highestValues,
+         'chartWidth': 1000,
+         'title': "Highest 5 Expenses"
+      },
+
+   ]
+
+   const [selectedValue, setSelectedValue] = useState(0);
+
+   const getVisualisationSelectedItem = (value) => {
+      setSelectedValue(value);
+   };
+
    return (
       <div>
+         <SelectVisualization visualisationSelectedItem={getVisualisationSelectedItem} />
          <div>
-            <Bar data={sorted_data} chartWidth={1400} title="All Expenses" />
+            <Bar data={barChartData[selectedValue].data} chartWidth={barChartData[selectedValue].chartWidth} title={barChartData[selectedValue].title} />
          </div>
+         {
+            /*
          <div>
-            <Bar data={lowestValues} chartWidth={1000} title="Lowest 5 Expenses" />
+            <Bar />
          </div>
 
          <div>
-            <Bar data={highestValues} chartWidth={1000} title="Highest 5 Expenses" />
+            <Bar />
          </div>
+         */
+         }
       </div>
    );
 };

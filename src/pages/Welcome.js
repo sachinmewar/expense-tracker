3869f4a4539d007a4446270ab6bbb931dae6748d
unknown <sachinmewar800@gmail.com>
2021-05-19 22:12:06 +0530

    Added Welcome page to Welcome.js

---
+++ src/pages/Welcome.js
@@ -0,0 +1,13 @@
+
+import './Welcome.css';
+
+const Welcome = () => {
+   return (
+      <div>
+         <h1> Welcome to expense-tracker Web App.</h1>
+         <h2> Please Login to continue. </h2>
+      </div>
+   );
+};
+
+export default Welcome;

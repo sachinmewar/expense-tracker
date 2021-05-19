ee0f0865a03eeb8a70dd0a8908bfef640dfdd74b
unknown <sachinmewar800@gmail.com>
2021-05-19 22:11:09 +0530

    Added localStorage to save state to auth-context.js

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-   const [token, setToken] = useState(null);
+   const initialToken = localStorage.getItem('token');
+   const [token, setToken] = useState(initialToken);
 
    const userIsLoggedIn = !!token;
 
    const loginHandler = (token) => {
       setToken(token);
+      localStorage.setItem('token', token);
    };
 
    const logoutHandler = () => {
       setToken(null);
+      localStorage.removeItem('token');
    };
 
    const contextValue = {

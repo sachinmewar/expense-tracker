86dc508175b74fb64319d5838a4eb3edc1e1e3c6
unknown <sachinmewar800@gmail.com>
2021-06-09 19:59:54 +0530

    firebase config files

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+   apiKey: "AIzaSyBsdhIpFdhOx4VMYlHYpdMoa4hbUMMcEAA",
+   authDomain: "expense-tracker-d14ff.firebaseapp.com",
+   projectId: "expense-tracker-d14ff",
+   storageBucket: "expense-tracker-d14ff.appspot.com",
+   messagingSenderId: "267293602060",
+   appId: "1:267293602060:web:bcadab132098be4c54d929"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
